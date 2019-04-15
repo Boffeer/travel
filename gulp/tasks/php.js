b9a2ea6acd127c351d78893c6b9f7de9b79b25d4
Boffeer <41575886+Boffeer@users.noreply.github.com>
2019-04-15 19:51:28 +0300

    Add files via upload

---
+++ gulp/tasks/php.js
@@ -0,0 +1,6 @@
+module.exports = function() {
+    $.gulp.task('php', () => {
+        return $.gulp.src('./dev/static/php/**/*.*')
+            .pipe($.gulp.dest('./build/static/php/'));
+    });
+};
