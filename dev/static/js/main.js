891e144072678e8ed007b61881543ea0d4711027
Nick <boffeechane@gmail.com>
2019-06-01 12:56:05 +0300

    complete pre-footer

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
     svg4everybody({});
 });
+
+
+// slider
+$(document).ready(function () {
+	$('.service-slider').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  cssEase: 'linear'
+	});
+});
